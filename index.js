b9957a088f5ba312a88481ed4916244ca59a8b49
enjoy301 <eunjong147@pusan.ac.kr>
2022-07-26 17:28:55 +0900

    feat: cli로 runtime, version 입력받기

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+console.clear();
+const runtime = await inquirer.prompt([
+  {
+    type: "list",
+    name: "runtime",
+    message: "런타임을 선택해 주세요.",
+    choices: ["Node.js", "python"],
+  },
+]);
+
+let version;
+if (runtime.runtime === "Node.js") {
+  version = await inquirer.prompt([
+    {
+      type: "list",
+      name: "version",
+      message: "버전을 선택해 주세요.",
+      choices: ["16.x", "14.x", "12.x"],
+    },
+  ]);
+} else {
+  version = await inquirer.prompt([
+    {
+      type: "list",
+      name: "version",
+      message: "버전을 선택해 주세요.",
+      choices: ["3.9", "3.8", "3.7", "3.6"],
+    },
+  ]);
+}
+
+switch (version.version) {
+  case "16.x":
+    console.log(1);
+    break;
+  case "14.x":
+    console.log(2);
+    break;
+  default:
+    console.log(3);
+}
